75fa5564e9f154e3d6aab6336a30087cc81cef7b
Silvyjson <139075245+Silvyjson@users.noreply.github.com>
2024-07-23 17:38:04 +0100

    added MIME type

--- Controllers/ReviewController/GetReviews.js
+++ Controllers/ReviewController/GetReviews.js
@@ -3,8 +3,10 @@ const ReviewModel = require("../../Models/ReviewModel");
 const handleGetReview = async (req, res) => {
   try {
     const review = await ReviewModel.find();
+    res.setHeader("Content-Type", "application/json");
     return res.status(200).json(review);
   } catch (error) {
+    res.setHeader("Content-Type", "application/json");
     return res.status(500).json({ message: error.message });
   }
 };
--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const app = express();
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({ origin: "*" }));
 
+app.use(
+  express.static(path.join(__dirname, "public"), {
+    setHeaders: (res, path) => {
+      if (path.endsWith(".js")) {
+        res.setHeader("Content-Type", "application/javascript");
+      }
+      if (path.endsWith(".css")) {
+        res.setHeader("Content-Type", "text/css");
+      }
+    },
+  })
+);
+
 const PORT = process.env.PORT || 8080;
 const MONGODB_URL = process.env.MONGODB_URL;
 
